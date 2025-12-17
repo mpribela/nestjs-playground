db35c8ec2ed62720c83c291e014ea12058b4d4ff
marek <m.pribela@gmail.com>
2025-12-17 20:24:44 +0100

    improved client type handling

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,29 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './controller/app.controller';
-import { AppService } from './service/app.service';
-import { AwsClient } from './client/aws.client';
-import { AzureClient } from './client/azure.client';
-import { CloudController } from './controller/cloud.controller';
-import { ConfigModule } from '@nestjs/config';
-import { FileService } from './service/file.service';
+import {Module} from '@nestjs/common';
+import {AppController} from './controller/app.controller';
+import {AppService} from './service/app.service';
+import {AwsClient} from './client/aws.client';
+import {AzureClient} from './client/azure.client';
+import {CloudController} from './controller/cloud.controller';
+import {ConfigModule} from '@nestjs/config';
+import {FileService} from './service/file.service';
+
+const getClientClass = () => {
+  if (!process.env.CLIENT) {
+    throw new Error(`Please define environment variable CLIENT`);
+  }
+  switch (process.env.CLIENT.toUpperCase()) {
+    case 'AWS':
+      return AwsClient;
+    case 'AZURE':
+      return AzureClient;
+    default:
+      throw new Error(`Unsupported client provided ${process.env.CLIENT}`);
+  }
+};
 
 const clientProvider = {
   provide: 'CLIENT',
-  useClass: process.env.CLIENT === 'AWS' ? AwsClient : AzureClient,
+  useClass: getClientClass(),
 };
 
 @Module({
